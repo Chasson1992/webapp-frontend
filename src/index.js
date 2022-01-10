6350a0d1bfec8c398778e3bdcb1067b8a34c16b1
Connor Hasson <connorkhasson@gmail.com>
2022-01-10 12:05:20 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloWorld from './HelloWorld';
+import UserList from './UserList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld text="Connor" />
+    <UserList />
   </React.StrictMode>,
   document.getElementById('root')
 );
